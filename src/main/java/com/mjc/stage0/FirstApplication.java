0b7a639fb6590c5f45c2f16c3980f70f9bdd08a6
Sarvarik <118633799+Sarvarik@users.noreply.github.com>
2022-11-20 03:04:41 +0500

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,30 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+ System.out.println("Fixed");
     }
 }
+
+public class PrintWordsApp {
+    public static void main(String[] args) {
+        System.out.println("java" + \n "is" + \n +"smth");
+    }
+}
+
+public class PrintSentenceApp {
+    public static void main(String[] args) {
+System.out.print("This is my first Java program");
+    }
+}
+
+public class BrokenApp {
+    public static void main(String[] args) {
+        System.out.println("fixMe");
+    }
+}
+
+public class UpdatedApp {
+    public static void main(String[] args) {
+        System.out.println("I'am updated");
+    }
+} 
