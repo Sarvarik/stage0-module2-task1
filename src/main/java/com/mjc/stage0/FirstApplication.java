c50e3b918ecc662ce38f57ae85167c3091c12061
Sarvarik <118633799+Sarvarik@users.noreply.github.com>
2022-11-26 23:01:06 +0500

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -4,3 +4,40 @@ public class FillGapsApp {
        System.out.println("Fixed");
     }
 }
+//2) Print words.
+//Make console output like: "java" + \n "is" + \n +"smth". Create java program that will write to console: java is smth
+
+//For that use the following code snippet (each word should begin with a new line):
+
+public class PrintWordsApp {
+    public static void main(String[] args) {
+System.out.println("java"+"\n is"+"\n smth");
+    }
+}
+//3) Print sentence.
+//Create main method and make console output: "This is my first Java program". Create java program that will write to console: This is my first Java program
+
+//For that use the following code snippet:
+
+public class PrintSentenceApp {
+    public static void main(String[] args){
+    System.out.println("This is my first Java program");
+    }
+}
+//4) Broken app.
+//Given piece of code (not compiling one), fill everything that is missing for the program and compile and build it. Do that inside of a code snippet here (each word should begin with a new line):
+
+public class BrokenApp {
+    public static void main(String[] args) {
+        System.out.println("fixMe");
+    }
+}
+//5) Update class.
+//Update this class to make an output: "I'm updated can be compiled. And can write to console"
+
+public class UpdatedApp {
+    public static void main(String[] args) {
+        System.out.println("I'm updated");
+    }
+}
+
