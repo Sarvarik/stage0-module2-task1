eeb26937c9d1bf4aca64b96c27e0b8f52bad45ec
Sarvarik <118633799+Sarvarik@users.noreply.github.com>
2022-11-26 22:55:25 +0500

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,6 @@
-package com.mjc.stage0;
-
-public class FirstApplication {
+//1) Fill in the gaps to make an application write data to console: fill the gaps to print "Fixed" to console with a new line. Use code snippet provided here (each word should begin with a new line):
+public class FillGapsApp {
     public static void main(String[] args) {
-       System.out.println("Hello, World!");
+       System.out.println("Fixed");
     }
 }
